4f8fa3ef186c2551e965d1ea4bb9476a77f7e5ac
PandyaDeval <dev23199@yahoo.in>
2024-10-09 11:42:26 -0700

    Added favicon path for google search

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,6 @@
 module.exports = eleventyConfig => {
 
   eleventyConfig.addPassthroughCopy("src/assets/**");
-  // eleventyConfig.addPassthroughCopy("src/google6673fe9fe660921f.html");
 
   return {
     dir: {
